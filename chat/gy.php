9678da5021c73c98fa20cb613490122930e3aa17
Mohammed Razaq <85779839+bnrazaq@users.noreply.github.com>
2022-09-09 08:24:57 -0600

    Create gy.php

---
+++ chat/gy.php
@@ -0,0 +1,26 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.6.1/firebase-app.js"></script>
+ 
+<!-- include firebase database -->
+<script src="https://www.gstatic.com/firebasejs/6.6.1/firebase-database.js"></script>
+ 
+<script>
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyAoZ5XVXwdE_cXwZ9yjZnBrZ93XbL1TNm0",
+        authDomain: "bnrazaq-2.firebaseapp.com",
+        databaseURL: "https://bnrazaq-2-default-rtdb.firebaseio.com",
+        projectId: "bnrazaq-2",
+        storageBucket: "bnrazaq-2.appspot.com",
+        messagingSenderId: "318840610848",
+        appId: "1:318840610848:web:1a8ee51eeaff2ce8e41c45"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    var myName = prompt("enter your name")
+</script>
+
+<form>
+    <input id="message" placeholder="enter massege" autocomplete="off">
+    
+</fotm>
